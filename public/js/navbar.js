fe94720dad5cfb2ddb516f422ef8c8e6661bc42d
Basile <basilealeveque@gmail.com>
2024-02-08 10:49:27 +0100

    micro modifs + section contact

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -8,6 +8,7 @@ function navFunction() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
         document.getElementById("navbar").style.backgroundColor = "#20202098";
+        document.getElementById("navbar").style.backdropFilter = "blur(10px)";
     } else {
         navbar.classList.remove("sticky");
         document.getElementById("navbar").style.backgroundColor = "#202020";
